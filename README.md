a6d2b9e2b6aa381cc34ea8f43ea84fcaffa95d8d
Shiv <shivam@felix.com>
2018-10-31 16:59:07 +0000

    conan configuration for Polars

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+
 # conan-polars
 A conan wrapper for https://github.com/felix-org/polars
+
+
+### Build instructions
+
+
+To add Polars to your conan cache:
+```sh
+$ conan create . user/channel -s cppstd=14
+```
+
+#### Dependencies
+Polars requires the following prebuilt package to be available for the environment you are building for in your local conan cache
+* `Armadillo/9.200.1@felix/stable`
+* `Date/2.4.1@felix/stable`
+
+:warning: Conan wrappers for these have not yet been published. [coming soon]
---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conans import ConanFile, CMake, tools
+
+
+class PolarsConan(ConanFile):
+    name = "Polars"
+    version = "0.1"
+    license = "MIT License"
+    url = "https://github.com/shivgadhia/polars"
+    description = "A C++ TimeSeries library that aims to mimic pandas Series"
+    settings = "cppstd", "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    build_policy = "missing"
+    requires = "Armadillo/9.200.1@felix/stable", "Date/2.4.1@felix/stable"
+
+    def source(self):
+        git = tools.Git()
+        git.clone("https://github.com/shivgadhia/polars.git", "cmake_dependency_flags")
+
+        # Add conan_basic_setup() to ensure resolved dependencies are linked
+        tools.replace_in_file("src/cpp/polars/CMakeLists.txt", "add_library(polars_cpp ${CPP_SOURCES})",
+        '''include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+        conan_basic_setup()
+        add_library(polars_cpp ${CPP_SOURCES})''')
+
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["WITH_TESTS"] = "OFF"
+        cmake.definitions["WITH_SUBMODULE_DEPENDENCIES"] = "OFF"
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src/cpp")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["polars_cpp"]
